2146ab6dae00acf33eeb20c0fa5b07a0500c1821
shruthi <shruthisa.17@gmail.com>
2023-11-16 21:15:09 -0800

    cleared out few unwanted code

--- js/script-ems.js
+++ js/script-ems.js
@@ -3,66 +3,40 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
 let form = document.querySelector('#addForm');
 let selectedTable = document.querySelector('#empTable');
-let tableBody = selectedTable.getElementsByTagName('tbody')[0];
-
-// STORAGE OBJECT
-let storage = "";
+let empCount = document.querySelector('#empCount');
 
 // INITIAL EMPLOYEES
-let employees = [];
+var employees = [];
+
+if( localStorage.getItem('employees') !== null ) {
+    employees = JSON.parse(localStorage.getItem('employees'));
+}
+else {
+    employees = [[123, "George", 3255, "g@gmail.com", "Executive"],
+    [ 234, "Christine", 3256, "c@gmail.com", "Engineering" ],
+    [ 345, "Zak", 3257, "z@gmail.com", "Sales" ],
+    [ 456, "Sally", 3258, "s@gmail.com", "Administrative" ]];
+}
 
 // LOAD EVENT
 window.addEventListener("load", (e) => {
-    employees = createInitialEmployees(employees);
-    buildGrid(employees);
-    addEmployee(employees);
-    deleteEmployee(employees);
+    buildGrid();
+    addEmployee();
+    deleteEmployee();
 });
 
-
-function createInitialEmployees(employees) {
-
-     // if there are no employees in employees array, check storage
-     if (employees.length === 0) {
-        // get tasks from storage or empty string if nothing in storage
-        storage = JSON.parse(localStorage.getItem('employees')) || "";
-
-        // if not empty, convert to array and store in global employees variable
-        if (storage.length > 0) {
-            storage.forEach(element => {
-                employees.push(element);
-            });
-        }
-
-        // if storage is also empty, then create initial set of employees and return the global employees array
-        else {
-            employees = [
-                    { id: 123, name:"George", extension:3255, email:"g@gmail.com", dept:"Executive"},
-                    { id: 234, name:"Christine", extension:3256, email:"c@gmail.com", dept:"Engineering" },
-                    { id: 345, name:"Zak", extension:3257, email:"z@gmail.com", dept:"Sales"},
-                    { id: 456, name:"Sally", extension:3258, email:"s@gmail.com", dept:"Administrative"}
-                ]
-            // ADD THE INITIAL EMPLOYEES TO THE LOCAL STORAGE 
-            localStorage.setItem('employees', JSON.stringify(employees));
-        }
-
-     }
-
-    return employees;
- 
-}
-
-
-function buildGrid(employees) {
+function buildGrid() {
     // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
     let count = 0;
 
     // CLEAR THE TABLE EVERYTIME AND BUILD GRID AFRESH, JUST TO AVOID ADDING THE EMPLOYEES ARRAY REPEATEDLY
-    tableBody.innerHTML = "";
+    selectedTable.lastElementChild.remove();
 
-    employees.forEach(emp => {
+    let tbody = document.createElement('tbody');
 
-        let row = tableBody.insertRow();
+      employees.forEach(emp => {
+
+        let row = tbody.insertRow();
         Object.values(emp).forEach(text => {
 
             let cell = row.insertCell();
@@ -73,24 +47,20 @@ function buildGrid(employees) {
         let cellDelete = row.insertCell();
         cellDelete.className = 'btn btn-sm btn-danger delete'; 
         cellDelete.innerHTML = 'X';
-        
+          
+      });
+
+        // BIND THE TBODY TO THE EMPLOYEE TABLE
+        selectedTable.appendChild(tbody);
+
         // INCREMENT THE NUMBER OF EMPLOYEES IN THE TABLE
-         count+= 1;
-         document.getElementById('empCount').value = count;
-    });
+        count = employees.length;
+        empCount.value = count;
+
   }
 
 
-function resetFormAndSetFocus() {
-        // RESET THE FORM
-        document.querySelector('#addForm').value = '';
-
-        // SET FOCUS BACK TO THE ID TEXT BOX
-        document.querySelector('#id').focus;
-}
-
-
-function addEmployee(employees) {
+function addEmployee() {
 // ADD EMPLOYEE
    form.addEventListener('submit', (e)=> { 
 
@@ -105,7 +75,7 @@ function addEmployee(employees) {
         let newDept = document.querySelector('#department').value;
 
         // CREATE NEW EMPLOYEE ARRAY USING THE VALUES EXTRACTED FROM THE TEXT BOXES
-        let newEmployee = {id: newId, name: newName, extension: newExtn, email: newEmail, dept: newDept};
+        let newEmployee = [newId, newName, newExtn, newEmail, newDept];
 
         // ADD THIS NEWLY ADDED EMPLOYEE TO THE MAIN EMPLOYEES ARRAY
         employees.push(newEmployee);
@@ -117,22 +87,23 @@ function addEmployee(employees) {
         buildGrid(employees);
 
         // CALL RESET THE FORM FIELDS METHOD
-        resetFormAndSetFocus();
+        form.reset();
+
+        // SET FOCUS BACK TO THE ID TEXT BOX
+        document.querySelector('#id').focus;
    });
 }
 
 
 // DELETE EMPLOYEE
-function deleteEmployee(employees) {
+function deleteEmployee() {
     selectedTable.addEventListener('click', (e)=> {
         if(confirm('Are you sure you want to delete the employee?')) {
 
-                console.log(e.target.parentElement);
                 let selectedRow = e.target.parentElement;
-                let cells = selectedRow.getElementsByTagName('td');
 
                 // DELETE THE SELECTED ROW IN THE TABLE
-                let rowIndex = selectedTable.deleteRow(e.target.parentElement.rowIndex);
+                let rowIndex = selectedTable.deleteRow(e.target.parentElement.parentElement.rowIndex);
 
                 // DELETE THE EMPLOYEE IN THE EMPLOYEES ARRAY THAT WAS SELECTED FOR DELETION
                 employees.splice(rowIndex, 1);
